f59cf834babccf5b0f4ef6482f5429d0f075b407
Amlakkolbe20 <Amlakkolbe20@users.noreply.github.com>
2024-08-19 23:48:11 +0000

    ✅ Ready to clone and code.

--- sa/ext/commands.py
+++ sa/ext/commands.py
@@ -1,8 +1,8 @@
 import click
 
-from project_name.ext.auth import create_user
-from project_name.ext.database import db
-from project_name.models import Product
+from sa.ext.auth import create_user
+from sa.ext.database import db
+from sa.models import Product
 
 
 def create_db():
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,9 @@
 import sys
 import pytest
 
-from project_name import create_app
-from project_name.ext.commands import populate_db
-from project_name.ext.database import db
+from sa import create_app
+from sa.ext.commands import populate_db
+from sa.ext.database import db
 
 
 @pytest.fixture(scope="session")
